32bad2c26299eb042602a2970e7afa1a2ec65a4a
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-22 16:45:39 +0200

    update: update link router

--- routes/api/link-router.js
+++ routes/api/link-router.js
@@ -3,7 +3,7 @@ import express from "express";
 import * as linkSchema from "../../models/Link.js";
 import linkController from "../../controllers/link-controller.js";
 
-import { authenticate, isValidId } from "../../middlewares/index.js";
+import { authenticate } from "../../middlewares/index.js";
 
 import { validateBody } from "../../decorators/index.js";
 
@@ -17,6 +17,6 @@ linkRouter.get("/", linkController.getAll);
 
 linkRouter.post("/", linkController.addLink);
 
-linkRouter.delete("/:contactId", isValidId, linkController.deleteById);
+linkRouter.delete("/:linkId", addLinkValidate, linkController.deleteById);
 
 export default linkRouter;
