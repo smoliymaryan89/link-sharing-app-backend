9171ebe063dcc01b535068f54593b68761fb8d65
Maryan Smoliy <112328727+smoliymaryan89@users.noreply.github.com>
2023-11-22 16:45:12 +0200

    update: update link controller

--- controllers/link-controller.js
+++ controllers/link-controller.js
@@ -13,21 +13,40 @@ const getAll = async (req, res) => {
 
 const addLink = async (req, res) => {
   const { id: owner } = req.user;
-  const result = await Link.create({ ...req.body, owner });
-  res.status(201).json(result);
+
+  const existingLink = await Link.findOne({ owner });
+
+  if (existingLink) {
+    const updatedLink = await Link.findOneAndUpdate({ ...req.body });
+
+    res.status(200).json(updatedLink);
+  } else {
+    const result = await Link.create({ ...req.body, owner });
+    res.status(201).json(result);
+  }
 };
 
 const deleteById = async (req, res) => {
   const { id: owner } = req.user;
-  const { contactId } = req.params;
+  const { linkId } = req.params;
 
-  const result = await Link.findOneAndDelete({ _id: contactId, owner });
+  const result = await Link.findOneAndUpdate(
+    { owner },
+    { $pull: { platform: { id: linkId } } }
+  );
 
   if (!result) {
     throw HttpError(404);
   }
 
-  res.json({ message: "link deleted" });
+  const link = await Link.findOne({ owner });
+
+  if (link.platform.length === 0) {
+    await Link.findOneAndDelete({ owner });
+    res.json({ message: "link and associated record deleted" });
+  } else {
+    res.json({ message: "link deleted" });
+  }
 };
 
 export default {
